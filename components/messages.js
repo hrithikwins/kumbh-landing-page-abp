b6c3a0bbe896760ce8eddf0245590b51a237e843
--get-all <javaoncloud14@gmail.com>
2025-01-16 10:19:07 +0530

    messages created

--- components/messages.js
+++ components/messages.js
@@ -1,6 +1,20 @@
 const messages = {
   en: {
+    heading: (
+      <>
+        THE PRESTIGIOUS
+        <br /> PRAYAGRAJ MAHAKUMBH MELA 2025
+      </>
+    ),
     about: "About",
+    aboutHeading:
+      <>
+        GET RID OF ALL YOUR SINS-
+        <br />
+        HOLY SNAN DATES
+      </>
+    ,
+    aboutSubheading: "BOOK YOUR SNAN DATES",
     planYourPilgrimage: "Plan Your Pilgrimage",
     mahakumbhCoverage: "Mahakumbh ABP Live Coverage",
     usefulLinks: "Useful Links",
@@ -11,7 +25,6 @@ const messages = {
     basantPanchami: "BASANT PANCHAMI",
     maghiPurnamasi: "MAGHI PURNAMASI",
     mahaShivaratri: "MAHA SHIVARATRI",
-    //  how to reach
     howToReach: "How to Reach",
     howToReachSubtitle: "Find detailed information on how to travel to Prayagraj using trains, flights, and buses.",
     whereToStay: "Where to Stay",
@@ -33,6 +46,73 @@ const messages = {
     foodAndThings: "Food and Things",
     allThingsKumbh: "All Things Kumbh",
     foodAttractions: "Food Attractions",
+    everythingYouNeed: "Everything You Need",
+    exploreServices: (
+      <>
+        Explore a variety of services designed to elevate your time in Prayagraj. You can take advantage of the amenities listed <br /> below to make your visit to Prayagraj even better. Feel free to choose the services that suit your preferences.
+      </>
+    ),
+    aboutHeading2: (
+      <>
+        humanity <br /> Gathering
+      </>
+    ),
+    spiritualSignificanceHeading: (
+      <>
+        Spiritual <br /> Significance
+      </>
+    ),
+    continuousCycleHeading: (
+      <>
+        Continuous <br /> Cycle
+      </>
+    ),
+
+    // News Section
+    newsSectionHeading: <>ABP News Coverage</>,
+    newsSectionDescription: <>We bring to you the all the latest updates about the Prayagraj Maha Kumbh 2025</>,
+    viewAllLatestNews: <>View all latest news</>,
+
+    // Mahakumbh Gallery
+    galleryHeading: <>Mahakumbh 2025 Gallery</>,
+    galleryDescription: <>We bring to you the all the latest photos & videos of the Prayagraj Maha Kumbh 2025</>,
+    viewAllGalleryUpdates: <>View all latest updates</>,
+
+    // Plan Your Pilgrimage
+    sarvsidhpradhKumbh: <>|| Sarvsidhpradh Kumbh ||</>,
+    acknowledgementsHeading: <>Majestic Acknowledgements & Testimonials</>,
+    pilgrimageHeading: <>Plan Your Pilgrimage</>,
+    pilgrimageDescription: <>Join us in this extraordinary journey and witness the confluence of tradition, devotion, and enlightenment.</>,
+    exploreOfferings: <>Explore our service offerings</>,
+
+    // How to Reach
+    howToReachHeading: <>How to reach?</>,
+    howToReachDescription: (
+      <>
+        Prayagraj city is well connected to all major Cities in India via the Indian <br /> Railways network, Highways and Prayagraj Airport located in Bamrauli.
+      </>
+    ),
+
+    // Tourist Guide
+    touristGuideHeading: <>Tourist guide</>,
+    touristGuideDescription: (
+      <>
+        The Tourism Department Uttar Pradesh has introduced 4 walks <br /> showcasing the various aspects of Prayagraj heritage.
+      </>
+    ),
+
+    // Where to Stay
+    whereToStayHeading: <>Where to stay?</>,
+    whereToStayDescription: (
+      <>
+        Tent city, hotels, public accommodations and home stay- Prayagraj <br /> Mahakumbh 2025 offers all types stays for you and your family.
+      </>
+    ),
+
+    // Everything You Need to Know
+    officialSiteLinkText: <>Visit official site of maha kumbh 2025</>,
+    experienceDescription: <>Experience the Prayagraj Mahakumbh Mela 2025</>,
+    advertiseWithUs: <>Advertise With Us</>,
   },
   hi: {
     about: "अधिक जानें",
@@ -47,27 +127,91 @@ const messages = {
     maghiPurnamasi: "माघी पूर्णिमा",
     mahaShivaratri: "महाशिवरात्रि",
     //how to reach
-     howToReach: "कैसे पहुँचें",
-  howToReachSubtitle: "प्रयागराज तक ट्रेन, फ्लाइट और बस से यात्रा करने की विस्तृत जानकारी प्राप्त करें।",
-  whereToStay: "कहाँ ठहरें",
-  whereToStaySubtitle: "टेंट सिटी और होटलों की जानकारी।",
-  directionToGhats: "घाटों का दिशा-निर्देश",
-  directionToGhatsSubtitle: "प्रयागराज के विभिन्न घाटों को जानें और गूगल मैप्स पर उनकी लोकेशन ढूंढें।",
-  touristGuide: "पर्यटक गाइड",
-  touristGuideSubtitle: "तीर्थयात्रियों और पर्यटकों के लिए प्रयागराज की समृद्ध विरासत और आध्यात्मिकता का प्रदर्शन।",
-  visitorsGuide: "विज़िटर गाइड",
-  visitorsGuideSubtitle: "भारत के अन्य प्रमुख शहरों से प्रयागराज कैसे पहुँचें।",
-  melaAttractions: "मेला आकर्षण",
-  melaAttractionsSubtitle: "कुंभ मेला क्षेत्र में मिलने वाले सभी आकर्षणों की सूची।",
-  nearbyAttractions: "पास के आकर्षण",
-  nearbyAttractionsSubtitle: "महा कुंभ मेले के लिए प्रयागराज में रहते हुए पास के दर्शनीय स्थलों का अन्वेषण करें।",
-  foodAndThings: "भोजन और चीज़ें",
+    howToReach: "कैसे पहुँचें",
+    howToReachSubtitle: "प्रयागराज तक ट्रेन, फ्लाइट और बस से यात्रा करने की विस्तृत जानकारी प्राप्त करें।",
+    whereToStay: "कहाँ ठहरें",
+    whereToStaySubtitle: "टेंट सिटी और होटलों की जानकारी।",
+    directionToGhats: "घाटों का दिशा-निर्देश",
+    directionToGhatsSubtitle: "प्रयागराज के विभिन्न घाटों को जानें और गूगल मैप्स पर उनकी लोकेशन ढूंढें।",
+    touristGuide: "पर्यटक गाइड",
+    touristGuideSubtitle: "तीर्थयात्रियों और पर्यटकों के लिए प्रयागराज की समृद्ध विरासत और आध्यात्मिकता का प्रदर्शन।",
+    visitorsGuide: "विज़िटर गाइड",
+    visitorsGuideSubtitle: "भारत के अन्य प्रमुख शहरों से प्रयागराज कैसे पहुँचें।",
+    melaAttractions: "मेला आकर्षण",
+    melaAttractionsSubtitle: "कुंभ मेला क्षेत्र में मिलने वाले सभी आकर्षणों की सूची।",
+    nearbyAttractions: "पास के आकर्षण",
+    nearbyAttractionsSubtitle: "महा कुंभ मेले के लिए प्रयागराज में रहते हुए पास के दर्शनीय स्थलों का अन्वेषण करें।",
+    foodAndThings: "भोजन और चीज़ें",
     travelRest: "Travel and Rest",
-  foodAndThingsSubtitle: "प्रयागराज और कुंभ मेले में उपलब्ध पाक व्यंजनों का आनंद लें।",
-  allServices: "All Services",
+    foodAndThingsSubtitle: "प्रयागराज और कुंभ मेले में उपलब्ध पाक व्यंजनों का आनंद लें।",
+    allServices: "All Services",
     foodAndThings: "Food and Things",
     allThingsKumbh: "All Things Kumbh",
     foodAttractions: "Food Attractions",
+    everythingYouNeed: "आपकी जरूरत की हर चीज",
+    exploreServices:
+      <>
+        प्रयागराज में अपने समय को बेहतर बनाने के लिए डिज़ाइन की गई विभिन्न सेवाओं का अन्वेषण करें। प्रयागराज की अपनी यात्रा को और भी बेहतर बनाने के लिए आप नीचे सूचीबद्ध <br />
+        सुविधाओं का लाभ उठा सकते हैं। अपनी पसंद के अनुसार सेवाएँ चुनने के लिए स्वतंत्र महसूस करें।
+      </>
+    ,
+    aboutHeading2:
+      <>
+        मानवता <br /> सभा
+      </>
+    ,
+    spiritualSignificanceHeading: (
+      <>
+        आध्यात्मिक <br /> महत्व
+      </>
+    ),
+    continuousCycleHeading: (
+      <>
+        निरंतर <br /> चक्र
+      </>
+    ),
+
+    // News Section
+    newsSectionHeading: <>एबीपी न्यूज़ कवरेज</>,
+    newsSectionDescription: <>हम आपको प्रयागराज महाकुंभ 2025 से जुड़ी सभी नवीनतम खबरें लाते हैं।</>,
+    viewAllLatestNews: <>सभी नवीनतम खबरें देखें</>,
+
+    // Mahakumbh Gallery
+    galleryHeading: <>महाकुंभ 2025 गैलरी</>,
+    galleryDescription: <>हम आपको प्रयागराज महाकुंभ 2025 की सभी नवीनतम तस्वीरें और वीडियो लाते हैं।</>,
+    viewAllGalleryUpdates: <>सभी नवीनतम अपडेट देखें</>,
+
+    // Plan Your Pilgrimage
+    sarvsidhpradhKumbh: <>|| सर्वसिद्धप्रद कुंभ ||</>,
+    acknowledgementsHeading: <>भव्य मान्यताएं और प्रशंसापत्र</>,
+    pilgrimageHeading: <>अपनी तीर्थयात्रा की योजना बनाएं</>,
+    pilgrimageDescription: <>इस असाधारण यात्रा में हमारे साथ जुड़ें और परंपरा, भक्ति और ज्ञान के संगम का साक्षी बनें।</>,
+    exploreOfferings: <>हमारी सेवा पेशकशों का अन्वेषण करें</>,
+
+    // How to Reach
+    howToReachHeading: <>कैसे पहुंचें?</>,
+    howToReachDescription: (
+      <>
+        प्रयागराज शहर भारत के सभी प्रमुख शहरों से भारतीय <br /> रेलवे नेटवर्क, राजमार्गों और बमरौली में स्थित प्रयागराज हवाई अड्डे के माध्यम से अच्छी तरह से जुड़ा हुआ है।
+      </>
+    ),
+
+    // Tourist Guide
+    touristGuideHeading: <>पर्यटक गाइड</>,
+    touristGuideDescription: <>उत्तर प्रदेश पर्यटन विभाग ने प्रयागराज विरासत के विभिन्न पहलुओं को दर्शाने वाले 4 वॉक पेश किए हैं।</>,
+
+    // Where to Stay
+    whereToStayHeading: <>कहां रुकें?</>,
+    whereToStayDescription: (
+      <>
+        टेंट सिटी, होटल, सार्वजनिक आवास और होम स्टे- प्रयागराज <br /> महाकुंभ 2025 आपके और आपके परिवार के लिए सभी प्रकार की आवास सुविधाएं प्रदान करता है।
+      </>
+    ),
+
+    // Everything You Need to Know
+    officialSiteLinkText: <>महाकुंभ 2025 की आधिकारिक साइट पर जाएं</>,
+    experienceDescription: <>प्रयागराज महाकुंभ मेला 2025 का अनुभव करें</>,
+    advertiseWithUs: <>हमारे साथ विज्ञापन करें</>,
   },
 };
 
