58bb79979f9b52acdbe467e8b3e1f5285b4466d5
--get-all <javaoncloud14@gmail.com>
2025-01-25 11:42:16 +0530

    locale set to hindi on submdomain by default

--- app/layout.js
+++ app/layout.js
@@ -1,28 +1,45 @@
-'use client'
+"use client";
 import "./globals.css";
-import React from 'react';
+import React, { useEffect } from "react";
 import { IntlProvider } from "react-intl";
 import { useState } from "react";
-import Head from 'next/head';
+import Head from "next/head";
 import messages from "../components/messages";
-import {LocaleProvider, useLocale} from "../components/contexts/LocaleContext"
+import {
+  LocaleProvider,
+  useLocale,
+} from "../components/contexts/LocaleContext";
 
-export default function RootLayout({
-  children,
-}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className='antialiased'>
+      <body className="antialiased">
         <LocaleProvider>
           <IntlWrapper>{children}</IntlWrapper>
         </LocaleProvider>
       </body>
     </html>
-  )
+  );
 }
 
 function IntlWrapper({ children }) {
-  const { locale } = useLocale();
+  const { locale, setLocale } = useLocale();
+
+  useEffect(() => {
+    // Ensure the code runs only in the browser
+    if (typeof window !== "undefined") {
+      const url = window.location.href; // Get the current URL
+      const hostname = new URL(url).hostname;
+      console.log("hostname", hostname);
+
+      if (hostname === "news.abplive.com") {
+        setLocale("hi"); // Hindi
+      } else {
+        setLocale("en"); // English
+      }
+    }
+  }, []);
+
   return (
     <IntlProvider messages={messages[locale]} locale={locale}>
       {children}
