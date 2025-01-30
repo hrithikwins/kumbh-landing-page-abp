857f9f0fe84564931b2435277380b289d4534c46
--get-all <javaoncloud14@gmail.com>
2025-01-30 11:55:35 +0530

    mahadev gif

--- components/success-dialog.jsx
+++ components/success-dialog.jsx
@@ -39,7 +39,7 @@ export function SuccessDialog({ open, onClose }) {
         <div className="text-center space-y-4">
           <div className="flex justify-center">
             <img
-              src="https://s3-alpha-sig.figma.com/img/0dd5/96b1/6fa87c1a3f037a37a60e6530c19df272?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=WXlVl9lhHsIxEnZe5Rgm5XXGgZ9fwJchcVvakOVmBZZQKY11guIi0r~WX06Dg~HeF2o4BHq9H1x2KDt7Pk-Mo3XAVsogJ6eZvnYZ-sciE8YESnjYcEEL-eH9mBa4fj~8HbhWJqa3akMBF6~7OWbwMYLG8kDJuuywIctmoUXFvH~wo-f9wsaBXBywt53z6H88l8zriiy7PT-Almel8frCCVdskpTwRA6L4-QsmzJrKpOgs19s1U5HOmK1CPdvY~1YHZh1B3pqCdilvoRNdrebuRHfc5ZanLHFilQF6jb4EAfyCRUotAReww16FFrzB7WIaJZBaMt659tFwcvnF53gQA__"
+              src="https://abp-mahakumbh-hindi.s3.us-east-1.amazonaws.com/mahadev-success.gif"
               alt="Success checkmark"
               className="w-32 h-32 object-contain mb-4"
             />
